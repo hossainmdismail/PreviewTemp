38304e5f0973c2f733cdee259a892be3d54b6a8d
esmail <hossainmdismail9010@gmail.com>
2024-03-05 11:27:09 +0600

    installed prev

---
+++ src/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  AOS.init({
+    duration: 400, // animation duration in milliseconds
+    easing: 'ease-in-out', // animation timing function
+  });
+});
